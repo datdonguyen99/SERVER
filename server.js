66c005e2c19659dc22b6a384c600535266ad72d1
unknown <datdonguyen99@gmail.com>
2022-06-16 22:01:51 +0700

    change-to-websocketstream

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-// MQTT broker
-const mosca = require("mosca");
-const settings = { port: 1357 };
-const server = new mosca.Server(settings);
+const aedes = require("aedes")();
+const httpServer = require("http").createServer();
+const ws = require("websocket-stream");
+const port = process.env.PORT || 80;
 
-server.on("ready", () => {
-  console.log("Server broker is ready!");
+ws.createServer({ server: httpServer }, aedes.handle);
+
+httpServer.listen(port, () => {
+  console.log("websocket server listening on port ", port);
 });
