ccbbdce5887dd3e68fbe4f7ddf87634bc0edf0b7
unknown <datdonguyen99@gmail.com>
2022-06-20 10:16:07 +0700

    connect-mongodb

---
+++ model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      default: "User0",
+    },
+    topic: {
+      type: String,
+      required: true,
+    },
+    temperature: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserMQTT = mongoose.model("UserMQTT", userSchema);
+
+module.exports = UserMQTT;
--- server.js
+++ server.js
@@ -1,10 +1,44 @@
+// MQTT Broker
 const aedes = require("aedes")();
+const server = require("net").createServer(aedes.handle);
 const httpServer = require("http").createServer();
 const ws = require("websocket-stream");
-const port = process.env.PORT || 80;
+require("dotenv").config();
+const wsPort = process.env.WSPORT || 1357;
+const port = process.env.PORT || 2468;
+const mongoose = require("mongoose");
+const UserMQTT = require("./model");
+
+server.listen(port, function () {
+  console.log("server started and listening on port ", port);
+});
 
 ws.createServer({ server: httpServer }, aedes.handle);
 
-httpServer.listen(port, () => {
-  console.log("websocket server listening on port ", port);
+httpServer.listen(wsPort, () => {
+  console.log("websocket server listening on port ", wsPort);
+});
+
+const connectDB = async (msg) => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    // console.log("Connected to mongodb successfully!");
+    const UserObject = new UserMQTT({
+      name: "DATDO",
+      topic: "TEMPERATURE",
+      temperature: msg,
+    });
+    UserObject.save();
+  } catch (err) {
+    console.log("Cannot connect to mongoDB", err);
+  }
+};
+
+aedes.subscribe("TEMPERATURE", (packet, callback) => {
+  const msg = packet.payload.toString();
+  // console.log(msg);
+  connectDB(msg);
 });
