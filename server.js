bbcc3c305df835916d4d8b0352f6bc970ef05a42
unknown <datdonguyen99@gmail.com>
2022-06-20 21:05:07 +0700

    check-socket

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // MQTT Broker
 const aedes = require("aedes")();
-const server = require("net").createServer(aedes.handle);
+// const server = require("net").createServer(aedes.handle);
 const httpServer = require("http").createServer();
 const ws = require("websocket-stream");
 require("dotenv").config();
 const wsPort = process.env.WSPORT || 80;
-const port = process.env.PORT || 2468;
-const mongoose = require("mongoose");
-const UserMQTT = require("./model");
+// const port = process.env.PORT || 2468;
+// const mongoose = require("mongoose");
+// const UserMQTT = require("./model");
 
-server.listen(port, function () {
-  console.log("server started and listening on port ", port);
-});
+// server.listen(port, function () {
+//   console.log("server started and listening on port ", port);
+// });
 
 ws.createServer({ server: httpServer }, aedes.handle);
 
@@ -19,26 +19,26 @@ httpServer.listen(wsPort, () => {
   console.log("websocket server listening on port ", wsPort);
 });
 
-const connectDB = async (msg) => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    // console.log("Connected to mongodb successfully!");
-    const UserObject = new UserMQTT({
-      name: "DATDO",
-      topic: "TEMPERATURE",
-      temperature: msg,
-    });
-    UserObject.save();
-  } catch (err) {
-    console.log("Cannot connect to mongoDB", err);
-  }
-};
+// const connectDB = async (msg) => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Connected to mongodb successfully!");
+//     const UserObject = new UserMQTT({
+//       name: "DATDO",
+//       topic: "TEMPERATURE",
+//       temperature: msg,
+//     });
+//     UserObject.save();
+//   } catch (err) {
+//     console.log("Cannot connect to mongoDB", err);
+//   }
+// };
 
-aedes.subscribe("TEMPERATURE", (packet, callback) => {
-  const msg = packet.payload.toString();
-  // console.log(msg);
-  connectDB(msg);
-});
+// aedes.subscribe("TEMPERATURE", (packet, callback) => {
+//   const msg = packet.payload.toString();
+//   console.log(msg);
+//   connectDB(msg);
+// });
